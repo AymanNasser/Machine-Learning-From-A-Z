244f8ffccff0faef3b4e26e14fff78966fd87cec
AymanNasser <aymannasr43@gmail.com>
2020-06-13 22:17:23 +0200

    Add .py file for SVR

---
+++ Regression/S9 - Support Vector Regression/svr.py
@@ -0,0 +1,7 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
