ef01a73c1261310a58ccfd283a4c491a22b2ff20
AymanNasser <aymannasr43@gmail.com>
2020-06-18 21:39:40 +0200

    Added Model selection & xgboost model

---
+++ Model Selection &  Boosting/S44 - XGBoost/xg_boost.py
@@ -0,0 +1,44 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.svm import SVC
+from xg
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Training the Kernel SVM model on the Training set
+classifier = SVC(kernel = 'rbf', random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting a new result
+print(classifier.predict(sc.transform([[30,87000]])))
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+#print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+# Making the Confusion Matrix & accuracy
+print(confusion_matrix(y_test, y_pred))
+print(accuracy_score(y_test, y_pred))
+
+# Applying K-Fold Cross Validation
+# cv: number of trained test folds
+accuracies = cross_val_score(estimator= classifier, X=X_train, y=y_train, cv= 10)
+print(accuracies)
+print('Accuracy: {:.2f} %'.format(accuracies.mean()*100))
+print('Standard Deviation: {:.2f} %'.format(accuracies.std()*100))
